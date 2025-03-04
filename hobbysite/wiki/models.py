54ac9fd0f738bad6d61cdfb9df7f190ec4566752
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-03-04 21:33:46 +0800

    Added ArticleCategory model

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField() # from https://www.geeksforgeeks.org/textfield-django-models/
+
+    class Meta:
+        ordering = ['name'] # Order by name in ascending order
+
+
+
+
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+class ArticleListView(ListView):
+    pass
+
 
 def index(request):
     return HttpResponse('Hello World! This is the Wiki app')
