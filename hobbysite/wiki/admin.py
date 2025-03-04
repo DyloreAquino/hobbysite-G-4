263bbd97587359c1d0b98a758f878167e99eb4d3
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-03-04 23:34:23 +0800

    Adjusted admin view

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 
 from .models import Article, ArticleCategory
 
-class ArticleAdminInline(admin.TabularInline):
-    model = Article
-    readonly_fields = ['created_on', 'updated_on']
-
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
-    inlines = [ArticleAdminInline]
 
     list_display = ('name', 'description')
 
@@ -19,3 +14,4 @@ class ArticleAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
