63ca80ab80ae121dbfdd77d343995875d6bd8f9e
bbiscuit0 <dannie.ruiz@student.ateneo.edu>
2025-03-05 12:54:29 +0800

    Created article_list and article_detail htmls

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import ArticleCategory, Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article_detail.html'
+
+
+def index(request):
+    return HttpResponse()
