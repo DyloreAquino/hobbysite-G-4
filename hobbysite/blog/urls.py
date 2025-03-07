3373fe809d0a5b2d01b4704806a7863671520a40
DyloreAquino <lutherjerold@gmail.com>
2025-03-07 20:27:43 +0800

    Removed index url from blog/urls.py

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import index, ArticleDetailView, ArticleListView
 
 urlpatterns = [
-    path('', index, name='index'),
     path('articles', ArticleListView.as_view(), name='article_list'),
     path('article/<int:pk>', ArticleDetailView.as_view(),
          name='article_detail')
