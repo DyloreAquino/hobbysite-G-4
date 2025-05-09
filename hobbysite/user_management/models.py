11b25d33cd62d42d2afc48d9312a2ee624abdc01
DyloreAquino <lutherjerold@gmail.com>
2025-05-10 01:27:16 +0800

    Modified Profile model to adhere to specs

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """Profile model for user"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    display_name = models.TextField(max_length=63, blank=False)
-    email_address = models.EmailField(blank=False)
+    display_name = models.CharField(max_length=63, blank=False)
+    email_address = models.EmailField()
