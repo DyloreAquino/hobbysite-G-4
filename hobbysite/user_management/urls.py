bda119df50afc494abac272b4f9ce8731abd5707
DyloreAquino <lutherjerold@gmail.com>
2025-05-10 01:29:05 +0800

    Fixed the links for display name update view

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ProfileUpdateView
 
 urlpatterns = [
-    path('username', ProfileUpdateView.as_view(), name='username'),
+    path('<str:user__username>', ProfileUpdateView.as_view(), name='username'),
 ]
 
 app_name = "user_management"
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -4,8 +4,14 @@ from django.views.generic.edit import UpdateView
 from .models import Profile
 from .forms import ProfileForm
 
+
 class ProfileUpdateView(UpdateView):
     model = Profile
-    template_name = 'user_management/username.html'
+    template_name = 'username.html'
+    
+    slug_field = 'user__username'
+    slug_url_kwarg = 'user__username'
+    
+    success_url = "/"
 
     form_class = ProfileForm
