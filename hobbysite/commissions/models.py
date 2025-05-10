b8a2f341476617c22f75821304840312667182a0
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-05-10 19:57:23 +0800

    Added status tab to Commissions model

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 from user_management.models import Profile
 # Create your models here.
 
+STATUS_CHOICES = (
+    ("OPEN", "Open"),
+    ("FULL", "Full"),
+    ("COMPLETE", "Completed"),
+    ("DISCONTINUED", "Discontinued")
+)
 
 class Commission(models.Model):
     """A model for commissions."""
@@ -14,6 +20,11 @@ class Commission(models.Model):
         on_delete=models.CASCADE,
     )
     description = models.TextField()
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='OPEN'
+    )
     requiredPeople = models.IntegerField()
     createdOn = models.DateTimeField()
     updatedOn = models.DateTimeField()
