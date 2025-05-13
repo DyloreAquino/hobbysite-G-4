59cf4a8afc9ac0010149ce2c230fcd3630806dc9
DyloreAquino <lutherjerold@gmail.com>
2025-05-13 12:10:50 +0800

    Added new models and modified older models for Merchstore

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
+
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default="No description available.")
@@ -20,6 +22,19 @@ class Product(models.Model):
     )
     description = models.TextField(default="No description available.")
     price = models.DecimalField(max_digits=63, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    
+    AVAILABLE = "available"
+    ONSALE = "on_sale"
+    OUTOFSTOCK = "out_of_stock"
+    
+    STATUS_CHOICES = (
+        (AVAILABLE, "Available"),
+        (ONSALE, "On Sale"),
+        (OUTOFSTOCK, "Out of Stock"),
+    )
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=AVAILABLE)
     
     class Meta:
         ordering = ['name']
@@ -29,3 +44,26 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('merchstore:product_detail', args=[self.pk])
+    
+class Transaction(models.Model):
+    buyer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    amount = models.PositiveIntegerField
+    
+    ONCART = "on_cart"
+    TOPAY = "to_pay"
+    TOSHIP = "to_ship"
+    TORECEIVE = "to_receive"
+    DELIVERED = "delivered"
+    
+    STATUS_CHOICES = (
+        (ONCART, "On Cart"),
+        (TOPAY, "To Pay"),
+        (TOSHIP, "To Ship"),
+        (TORECEIVE, "To Receive"),
+        (DELIVERED, "Delivered"),
+    )
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    
+    created_on = models.DateTimeField(auto_now_add=True)
