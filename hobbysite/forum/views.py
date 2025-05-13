380f03205699f655433c3d746d44a91cb461cd78
kp-04z <kijrl.pagtulon-an@student.ateneo.edu>
2025-05-13 18:32:45 +0800

    created appropriate class-based views

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import PostCategory, Post
+from .models import ThreadCategory, Thread, Comment
+
+class ThreadListView(ListView):
+    model = Thread
+    template_name = 'forum/thread_list.html'
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['categories'] = ThreadCategory.objects.all()
+        return ctx
+
+class ThreadDetailView(DetailView):
+    model = Thread
+    template_name = 'forum/thread_detail.html'
 
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'forum/post_list.html'
+class ThreadCreateView(LoginRequiredMixin, CreateView):
+    model = Thread
+    template_name = 'forum/thread_detail.html'
+    
 
-
-class PostDetailView(LoginRequiredMixin, DetailView):
-    model = Post
-    template_name = 'forum/post_detail.html'
+class ThreadUpdateView(LoginRequiredMixin, UpdateView):
+    model = Thread
+    template_name = 'forum/thread_detail.html'
