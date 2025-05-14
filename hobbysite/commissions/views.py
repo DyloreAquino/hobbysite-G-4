6c53f3fbd88bcf7f4fdb04b36ba9cf5b38769fef
Joaquin Alonzo A. Salazar <alonzo.salazar@student.ateneo.edu>
2025-05-14 15:13:41 +0800

    AddJob form included and functional

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -3,13 +3,12 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from .forms import CommissionAddForm, JobAddForm
-from .models import Commission, Job
+from .models import Commission
 
 
 class CommissionListView(ListView):
@@ -40,7 +39,7 @@ def handle_commission_add_page(request):
             pk = comm_form.instance.id
         
         if job_form.is_valid():
-            job_form.instance.commission = request.POST.instance
+            job_form.instance.commission = comm_form.instance
             job_form.save()
 
         return redirect(reverse('commissions:commissions-detail', args=[pk]))
@@ -50,27 +49,5 @@ def handle_commission_add_page(request):
 
     return render(request, 'commissions/commissions_create.html', {
         'comm_form': comm_form,
-        'job_form': comm_form,
+        'job_form': job_form,
     })
-
-"""
-class CommissionCreateView(LoginRequiredMixin, CreateView):
-    model = Commission
-    template_name = 'commissions/commissions_create.html'
-    form_class = CommissionAddForm
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx['formset'] = JobFormSet()
-        ctx['comm_form'] = CommissionAddForm()
-        return ctx
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
-
-    def form_valid(self, comm_form):
-        comm_form.instance.author = self.request.user.profile
-        return super().form_valid(comm_form)
-"""
