caf2fc540a5069f3b573a9a3cdc0ba446f7a72e8
bbiscuit0 <dannie.ruiz@student.ateneo.edu>
2025-05-14 23:17:15 +0800

    Updated urls.py to contain newly created views

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,11 +1,25 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
-from .views import index, ArticleDetailView, ArticleListView
+from .views import (
+    ArticleDetailView,
+    ArticleListView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleAddImageView,
+)
+
 
 urlpatterns = [
     path('articles', ArticleListView.as_view(), name='article_list'),
     path('article/<int:pk>', ArticleDetailView.as_view(),
-         name='article_detail')
+         name='article_detail'),
+    path('article/add', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/edit',
+         ArticleUpdateView.as_view(), name='article_update'),
+    path('article/<int:pk>/add_image',
+         ArticleAddImageView.as_view(), name='article_addimage'),
+
 ]
 
 app_name = "blog"
