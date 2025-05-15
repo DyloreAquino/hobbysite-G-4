8c27890c7c29d79974cd1ffcfa2b56c73e740c25
DyloreAquino <lutherjerold@gmail.com>
2025-05-15 18:42:14 +0800

    Fixed admin.py to fit PEP8standards

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
 from .models import ProductType, Product, Transaction
 
+
 class ProductInline(admin.TabularInline):
     model = Product
+
 
 class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
     inlines = [ProductInline,]
 
+
 class ProductAdmin(admin.ModelAdmin):
     model = Product
 
+
 class TransactionAdmin (admin.ModelAdmin):
     model = Transaction
+
 
 admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Product, ProductAdmin)
