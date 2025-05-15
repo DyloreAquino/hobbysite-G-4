a89594280cd32f663026def4fd1413731f8249cf
bbiscuit0 <dannie.ruiz@student.ateneo.edu>
2025-05-15 18:28:02 +0800

    Edited for PEP8

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -24,11 +24,12 @@ urlpatterns = [
     path('forum/', include('forum.urls', namespace='forum')),
     path('blog/', include('blog.urls', namespace="blog")),
     path('wiki/', include('wiki.urls', namespace="wiki")),
-    path('merchstore/', include('merchstore.urls', namespace = 'merchstore')),
+    path('merchstore/', include('merchstore.urls', namespace='merchstore')),
     path('commissions/', include('commissions.urls', namespace="commissions")),
     path('', include('homepage.urls', namespace='homepage')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('profile/', include('user_management.urls', namespace='user_management')),
+    path('profile/', include('user_management.urls',
+                             namespace='user_management')),
     path('admin/', admin.site.urls),
 ]
 
