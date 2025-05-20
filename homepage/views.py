7aaae6313b1752d3b216cd43cca9f102fba272dc
kp-04z <kijrl.pagtulon-an@student.ateneo.edu>
2025-05-20 09:39:29 +0800

    creates profile for users with no profile

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+
+from django.contrib.auth.models import User
+from user_management.models import Profile
+
+
 def homepage(request):
     context = {}
-    return render(request, 'homepage.html', context)
 
-# Create your views here.
+    for user in User.objects.all():
+        if not hasattr(user, 'profile'):
+            user.profile = Profile()
+            user.profile.display_name = user.username
+            user.profile.email_address = user.email
+            user.profile.save()
+
+    return render(request, 'homepage.html', context)
